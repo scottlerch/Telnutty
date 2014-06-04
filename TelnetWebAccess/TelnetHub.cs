80e007a69a8a5758b0d7dfda9ec6c4c1814d5bb2
Scott Lerch <scottlerch@gmail.com>
2014-06-04 13:55:23 -0700

    Handle tab, arrow up/down, and backspace.

--- TelnetWebAccess/TelnetHub.cs
+++ TelnetWebAccess/TelnetHub.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -6,19 +8,101 @@ using Microsoft.AspNet.SignalR;
 
 namespace TelnetWebAccess
 {
+    public enum JavaScriptKeyCodes
+    {
+        Left = 37,
+        Up = 38,
+        Right = 39,
+        Down = 40,
+        Tab = 9,
+        Backspace = 8,
+        Enter = 13,
+    }
+
     public class TelnetHub : Hub
     {
+        private static readonly HashSet<char> PrintableChars = new HashSet<char>(
+            Enumerable
+            .Range(0, char.MaxValue + 1)
+                .Select(i => (char) i)
+                .Where(c => !char.IsControl(c))
+                .Except(new[]
+                {
+                    (char)JavaScriptKeyCodes.Left, 
+                    (char)JavaScriptKeyCodes.Up,
+                    (char)JavaScriptKeyCodes.Right,
+                    (char)JavaScriptKeyCodes.Down,
+                    (char)JavaScriptKeyCodes.Enter,
+                }));
+
         private static readonly object tcpClientLock = new object();
         private static TcpClient tcpClient;
         private static Stream tcpStream;
         private static Task tcpReaderTask;
 
-        public void Send(int keyCode)
+        public void SendKeyPress(int keyCode)
+        {
+            if (PrintableChars.Contains((char)keyCode))
+            {
+                Write(Encoding.UTF8.GetBytes(new[] {(char) keyCode}));
+            }
+        }
+
+        private static void Write(byte[] data)
         {
             lock (tcpClientLock)
             {
-                var data = Encoding.UTF8.GetBytes(new[] {(char) keyCode});
+                if (tcpStream == null) return;
                 tcpStream.Write(data, 0, data.Length);
+            }
+        }
+
+        private static void WriteEscapeSequence(params byte[] values)
+        {
+            var fullEscapeSequence = new byte[values.Length + 2];
+            fullEscapeSequence[0] = 0x1b;
+            fullEscapeSequence[1] = 0x5b;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                fullEscapeSequence[i + 2] = values[0];
+            }
+
+            Write(fullEscapeSequence);
+        }
+
+        public void SendKeyDown(int keyCode)
+        {
+            if (!PrintableChars.Contains((char) keyCode))
+            {
+                switch ((JavaScriptKeyCodes)keyCode)
+                {
+                    case JavaScriptKeyCodes.Tab:
+                        WriteEscapeSequence(0x09);
+                        break;
+
+                    case JavaScriptKeyCodes.Backspace:
+                        WriteEscapeSequence(0x7f);
+                        break;
+
+                    case JavaScriptKeyCodes.Enter:
+                        Write(Encoding.UTF8.GetBytes(new [] {'\r'}));
+                        break;
+
+                    case JavaScriptKeyCodes.Left:
+                        break;
+
+                    case JavaScriptKeyCodes.Right:
+                        break;
+
+                    case JavaScriptKeyCodes.Up:
+                        WriteEscapeSequence(0x41);
+                        break;
+
+                    case JavaScriptKeyCodes.Down:
+                        WriteEscapeSequence(0x42);
+                        break;
+                }
             }
         }
 
@@ -47,7 +131,11 @@ namespace TelnetWebAccess
                             try
                             {
                                 var count = tcpStream.Read(buffer, 0, buffer.Length);
-                                Clients.All.addText(Encoding.UTF8.GetString(buffer, 0, count));
+
+                                if (count > 0)
+                                {
+                                    Clients.All.addKeyCodes(buffer.Take(count).Select (x => (int)x).ToArray());
+                                }
                             }
                             catch
                             {
