bf1e248565cb8471c8b181f1e8f852ffe859e630
Scott Lerch <scottlerch@gmail.com>
2014-06-05 11:17:53 -0700

    Specify host and port with query parameters.

--- TelnetWebAccess/App_Start/BundleConfig.cs
+++ TelnetWebAccess/App_Start/BundleConfig.cs
@@ -19,6 +19,9 @@ namespace TelnetWebAccess
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryplugins").Include(
+                        "~/Scripts/purl.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
