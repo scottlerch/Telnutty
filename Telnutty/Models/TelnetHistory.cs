9b913bad92ea705a54d55f2ea6f7604b79e3077d
Scott Lerch <scottlerch@gmail.com>
2014-06-24 19:59:05 -0700

    Disable history until cloud friendly replacement found (can't write to App_Data on Azure websites).

--- Telnutty/Models/TelnetHistory.cs
+++ Telnutty/Models/TelnetHistory.cs
@@ -1,50 +1,50 @@
 ﻿using System;
-using System.IO;
-using System.Web;
+//using System.IO;
+//using System.Web;
 
 namespace Telnutty.Models
 {
     public class TelnetHistory
     {
-        private readonly string historyPath;
+        // private readonly string historyPath;
 
         public TelnetHistory(TelnetEndPoint endPoint)
         {
             if (endPoint == null) throw new ArgumentNullException("endPoint");
 
-            historyPath = HttpContext.Current.Server.MapPath("~/App_Data/" + Guid.NewGuid().ToString("N") + "_" + endPoint + ".dat");
+            // historyPath = HttpContext.Current.Server.MapPath("~/App_Data/" + Guid.NewGuid().ToString("N") + "_" + endPoint + ".dat");
         }
 
         public byte[] GetHistory(int sizeInBytes)
         {
-            if (sizeInBytes < 0) throw new ArgumentException("sizeInBytes must be greater than or equal to 0");
+            //if (sizeInBytes < 0) throw new ArgumentException("sizeInBytes must be greater than or equal to 0");
 
-            if (File.Exists(historyPath))
-            {
-                using (var fileStream = File.Open(historyPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-                {
-                    fileStream.Position = Math.Max(fileStream.Length - sizeInBytes, 0);
+            //if (File.Exists(historyPath))
+            //{
+            //    using (var fileStream = File.Open(historyPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            //    {
+            //        fileStream.Position = Math.Max(fileStream.Length - sizeInBytes, 0);
 
-                    var data = new byte[fileStream.Length - fileStream.Position];
-                    fileStream.Read(data, 0, data.Length);
-                    fileStream.Close();
+            //        var data = new byte[fileStream.Length - fileStream.Position];
+            //        fileStream.Read(data, 0, data.Length);
+            //        fileStream.Close();
 
-                    return data;
-                }
-            }
+            //        return data;
+            //    }
+            //}
 
             return new byte[0];
         }
 
         public void AppendHistory(byte[] data)
         {
-            if (data == null) throw new ArgumentNullException("data");
+            //if (data == null) throw new ArgumentNullException("data");
 
-            using (var fileStream = File.Open(historyPath, FileMode.Append, FileAccess.Write, FileShare.Read))
-            {
-                fileStream.Write(data, 0, data.Length);
-                fileStream.Close();
-            }
+            //using (var fileStream = File.Open(historyPath, FileMode.Append, FileAccess.Write, FileShare.Read))
+            //{
+            //    fileStream.Write(data, 0, data.Length);
+            //    fileStream.Close();
+            //}
         }
     }
 }
